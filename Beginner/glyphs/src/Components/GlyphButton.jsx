18d12df777c739198814d777d0474c455e30ad9d
Manish Kumar <manishkumar9782@gmail.com>
2023-02-15 16:01:23 +0530

    worked on the glyph game

--- Beginner/glyphs/src/Components/GlyphButton.jsx
+++ Beginner/glyphs/src/Components/GlyphButton.jsx
@@ -1,48 +1,63 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from "react";
 
 const images = process.env.PUBLIC_URL + "/Images/Remnant Glyphs";
 const imagesArray = [
-    "/glyphs 1.png",
-    "/glyphs 2.png",
-    "/glyphs 3.png",
-    "/glyphs 4.png",
-    "/glyphs 5.png",
+  "/glyphs 1.png",
+  "/glyphs 2.png",
+  "/glyphs 3.png",
+  "/glyphs 4.png",
+  "/glyphs 5.png",
 ];
 
 const GlyphButton = (props) => {
+  const [imgIndex, setImg] = useState(props.glyphValue);
+  const ref_state = useRef();
+  const btn_ref = useRef();
 
-    const [imgIndex, setImg] = useState(props.glyphValue)
+  function onClickHandler() {
+    console.log("selected image: " + imgIndex);
 
-
-    function onClickHandler() {
-
-        console.log("selected image: " + imgIndex);
-        // testing for ending position for imagesArray.
-        if ((imgIndex + 1) % (imagesArray.length) === 0) {
-            // this condition is true it means we have reached the end of the imagesArray.
-            setImg(0);
-        }
-        else {
-            setImg(imgIndex + 1);
-        }
-
-        // Now we will update our glyph matrix in which we have all glyphs record.
-        let newMatrix = new Array(...props.glyphMatrix);
-        newMatrix[props.id] = imgIndex;
-        props.setGlyphMatrix(newMatrix);
+    if (ref_state !== null) {
+      ref_state.current.style.filter = "hue-rotate(200deg)";
     }
 
-    return (
-        <button type='button'>
-            <img className='glyph-button' src={images + imagesArray[imgIndex]} alt=""
-                // here src image will be set by using the 'images' value which string and 
-                // we will pass the index number to the 'imagesArray' to get the current image 
-                // for this button, imagesArray[imgIndex] will give us th image name which is added
-                // to the images variable to get the final src value.
-                onClick={onClickHandler}
-            />
-        </button>
-    )
-}
+    // testing for ending position for imagesArray.
+    if ((imgIndex + 1) % imagesArray.length === 0) {
+      // this condition is true it means we have reached the end of the imagesArray.
+      setImg(0);
+    } else {
+      setImg(imgIndex + 1);
+    }
 
-export default GlyphButton
+    // Now we will update our glyph matrix in which we have all glyphs record.
+    let newMatrix = new Array(...props.glyphMatrix);
+    newMatrix[props.id] = imgIndex;
+    props.setGlyphMatrix(newMatrix);
+  }
+
+  return (
+    <button ref={btn_ref} type="button" onClick={onClickHandler}>
+      {imgIndex != null ? (
+        <img
+          ref={ref_state}
+          className="glyph-button"
+          src={images + imagesArray[imgIndex]}
+          alt=""
+          // here src image will be set by using the 'images' value which string and
+          // we will pass the index number to the 'imagesArray' to get the current image
+          // for this button, imagesArray[imgIndex] will give us th image name which is added
+          // to the images variable to get the final src value.
+        />
+      ) : (
+        <img
+          ref={ref_state}
+          className="glyph-button"
+          src={images + "/white.png"}
+          alt=""
+        />
+      )}
+    </button>
+  );
+};
+
+export default GlyphButton;
--- Beginner/glyphs/src/Components/GlyphGame.jsx
+++ Beginner/glyphs/src/Components/GlyphGame.jsx
@@ -4,9 +4,22 @@ import GlyphsGrid from './GlyphsGrid'
 import SubmitGlyph from './SubmitGlyph';
 
 const glyphMatrix = [
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+    0, null, 1, null, 4,
+    null, 2, null, 1, null,
+    null, null, 3, null, null,
+    null, 4, null, 0, null,
+    1, null, 4, null, 2,
 ];
 
+/**
+ * 1 n 2 n 5
+ * n 3 n 2 n
+ * n n 4 n n
+ * n 5 n 1 n 
+ * 2 n 5 n 3
+ * 
+ * 
+ */
 
 const GlyphGame = () => {
     const [glyphs, setGlyphs] = useState(glyphMatrix);
