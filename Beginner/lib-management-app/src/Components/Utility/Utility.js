69b114d57a12f402a109bca789804b79a561f50c
Manish Kumar <manishkumar9782@gmail.com>
2023-03-16 15:50:43 +0530

    worked on the Student Class and localStorage Database

--- Beginner/lib-management-app/src/Components/Utility/Utility.js
+++ Beginner/lib-management-app/src/Components/Utility/Utility.js
@@ -12,7 +12,10 @@ export function getItem(key) {
      * this function will be used to get the data in key value pair form local storage.
      */
     let data = localStorage.getItem(key)
-    return JSON.parse(data)
+    if (data) {
+        return JSON.parse(data)
+    }
+    return []
 }
 
 export function getFormData(form, inputs) {
@@ -43,19 +46,40 @@ export function makeFormEntry(e, form) {
     // then set an empty array into localStorage
     // using the database key (Students)
     if (getItem(form.dataset.database) == null) {
-        setItem(form.dataset.database, []);
+        setItem(form.dataset.database, {
+            fields: Student.fields,
+            students: []
+        });
     }
 
     // now retrieve the database from localStorage.
     database = getItem(form.dataset.database);
 
-    // add the new entry to the database.
-    database.push(
-        getFormData(form, form.dataset.inputNames.split(","))
-    );
+    Student.Fields = database.fields
 
-    // Now after updating our database we need to set the database.
-    setItem(form.dataset.database, database);
+    database.students.forEach((item) => {
+        let st = new Student()
+        Student.fields.forEach((key, index) => {
+            // updating each field, where key is the field name,
+            // and item[index] will be our value
+            st.updateField(key, item[index])
+        })
+    })
+
+    // // add the new entry to the database.
+    // database.push(
+    //     getFormData(form, form.dataset.inputNames.split(","))
+    // );
+
+    // // Now after updating our database we need to set the database.
+    // setItem(form.dataset.database, database);
+    let data = getFormData(form, form.dataset.inputNames.split(","))
+    new Student({
+        name: data.name, email: data.email, contact: data.contact,
+        address: data.address, _class: data.class
+    })
+
+    Student.saveStudents()
 
     // after submitting the form data we need to reset the form
 
@@ -73,30 +97,67 @@ export function getItemList(database) {
 
 class Student {
 
-    static Students;
+    // list of all Student Objects
+    static Students = [];
+    // Student Database name in localStorage
+    static DatabaseKey = 'Students';
 
-    constructor({ id, name, _class, email, contact, address, current_issued_book, registration_date, updated_on, is_finned, finned_amount, is_returned, returned_date, Image }) {
+    // current student id for new student.
+    static StudentId = 1;
 
-        this.id = id;
+    // fields which are loaded and stored inside the localStorage.
+    static Fields = [
+        "id", "name", "class", "email", "contact",
+        "address", "current_issued_book", "registration_date", "updated_on", "is_finned", "finned_amount", "is_returned", "returned_date", "Image"
+    ]
+
+    // a constructor to create a new student object.
+    constructor({ name, _class, email, contact, address, current_issued_book, registration_date, updated_on, is_finned, finned_amount, is_returned, returned_date, Image }) {
+
+        this.id = Student.StudentId++;
         this.name = name;
         this.class = _class;
         this.email = email;
         this.contact = contact;
         this.address = address;
         this.current_issued_book = current_issued_book;
-        this.registration_date = registration_date;
-        this.updated_on = updated_on;
+        this.registration_date = new Date().toLocaleDateString();
+        this.updated_on = new Date().toLocaleDateString;
         this.is_finned = is_finned;
         this.finned_amount = finned_amount;
         this.is_returned = is_returned;
         this.returned_date = returned_date;
         this.Image = Image;
+        Student.Students.push(this)
 
     }
 
-    update(key, value) {
-        this.key = value;
+    updateField(key, value) {
+        this[key] = value;
     }
 
+    static loadStudents() {
+        let data = getItem(Student.DatabaseKey)
+    }
+
+    static saveStudents() {
+        // Now since our data will be stored in localStorage, which need string
+        // representation of our data.
+
+        // now we will get all the student values.
+        setItem(Student.DatabaseKey, {
+            fields: Student.Fields,
+            students: Student.Students.map((item) => item.toArray())
+        })
+    }
+
+
+    toArray() {
+        /**
+         *  "id", "name", "class", "email", "contact",
+        "address", "current_issued_book", "registration_date", "updated_on", "is_finned", "finned_amount", "is_returned", "returned_date", "Image"
+         */
+        return [this.id, this.name, this.class, this.email, this.contact, this.address, this.is_finned, this.current_issued_book, this.registration_date, this.updated_on, this.is_finned, this.finned_amount, this.is_returned, this.returned_date, this.Image]
+    }
 
 }   
